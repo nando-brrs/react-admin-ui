b147410456902e0973004e209dfbadb6b6862a8a
Fernando Barros <nando.brrs@gmail.com>
2023-07-21 09:06:49 -0300

    adicionando estilos para navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,9 +1,28 @@
-import "./navbar.scss";
+    import "./navbar.scss";
 
-const NavBar = () => {
-    return (
-        <div className="navbar">navbar</div>
-    )
-}
+    const NavBar = () => {
+        return (
+            <div className="navbar">
+                <div className="logo">
+                    <img src="logo.svg" ></img>
+                    <span>LamaAdmin</span>
+                </div>
+                <div className="icons">
+                <img src="/search.svg" alt=""></img>
+                <img src="/app.svg" alt=""></img>
+                <img src="/expand.svg" alt=""></img>
+                <div className="notification">
+                    <img src="/notifications.svg" alt=""></img> 
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img src="/noavatar.png" alt=""></img> 
+                    <span>Jane</span>
+                </div>
+                <img src="/settings.svg" alt=""></img>
+                </div>
+            </div>
+        )
+    }
 
-export default NavBar;
+    export default NavBar;
